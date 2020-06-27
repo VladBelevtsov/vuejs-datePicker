b2357c76f0d678ac2ff52461bd6a97e7db8a75c8
Vlad Bielievtsov <vladbelevtsovdaime@gmail.com>
2020-06-27 18:04:56 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,135 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <datePicker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import datePicker from './components/datePicker.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    datePicker
   }
+}
+var state = {
+  date: new Date(2020, 6,  16)
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0px;
+  padding: 0px;
+  font-family: sans-serif;
 }
+  #app{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background: #f1f1f1;
+  }
+  $borderRadius: 20px;
+  $borderRadiusShadow: 16px;
+  $colorPrimary: #455AF7;
+  .card {
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    width: 300px;
+    height: 400px;
+    border-radius: $borderRadius;
+    box-shadow: 0 24px 38px 3px rgba(0, 0, 0, 0.025),
+      0 9px 46px 8px rgba(0, 0, 0, 0.025), 0 11px 15px -7px rgba(0, 0, 0, 0.025);
+  }
+  .card-footer {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0 0 1rem;
+    color: #666;
+    background: #F5F6FA;
+  }
+  .card-footer__icon {
+    font-size: 29px;
+    margin-bottom: 0.5rem;
+  }
+  .card-footer__text {
+    font-weight: 700;
+    font-size: 0.875rem;
+    color: #666;
+  }
+  .fab-button {
+    position: absolute;
+    right: 20px;
+    bottom: -20px;
+    width: 44px;
+    height: 44px;
+    border-radius: 50%;
+    border: 0;
+    background: $colorPrimary;
+    color: white;
+    font-family: 'Material Icons';
+  }
+  @mixin shadow-defaults {
+    content: "";
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    background: rgba(255, 255, 255, 0.25);
+  }
+  .datepicker-wrapper {
+    position: relative;
+    z-index: 2;
+    padding: 1rem;
+    border-radius: $borderRadius;
+    background: #fff;
+    box-shadow: 0 24px 38px 3px rgba(0, 0, 0, 0.025);
+    transform: translateY($borderRadius);
+    margin-top: -$borderRadius;
+    .vdp-datepicker__calendar {
+      background: inherit;
+      width: 100%;
+      border: 0;
+      font-size: 0.875rem;
+      color: rgba(#090c22, 0.85);
+    }
+    .vdp-datepicker__calendar header span {
+      font-weight: 700;
+    }
+    .vdp-datepicker__calendar .cell:not(.month):not(.year) {
+      line-height: 38px;
+      height: 38px;
+      border-radius: 35%;
+    }
+    .vdp-datepicker__calendar .cell:not(.blank):not(.disabled).month:hover {
+      border-color: $colorPrimary;
+    }
+    .vdp-datepicker__calendar .cell.selected,
+    .vdp-datepicker__calendar .cell.selected:hover {
+      background: $colorPrimary;
+      color: white;
+    }
+    .vdp-datepicker__calendar .cell:not(.blank):not(.disabled).day:hover {
+      background: #d9ddeb;
+      border: 0;
+    }
+    .vdp-datepicker__calendar header .prev:after {
+      border-right-color: $colorPrimary;
+      font-family: 'Material Icons';
+    }
+    .vdp-datepicker__calendar header .next:after {
+      border-left-color: $colorPrimary;
+      font-family: 'Material Icons';
+    }
+    .vdp-datepicker__calendar .cell.day-header {
+      opacity: 0.57;
+    }
+  }
+
 </style>
---
+++ src/components/datePicker.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="card">
+    <div class="datepicker-wrapper">
+      <button class="fab-button material-icons-outlined">add</button>
+      <Datepicker value mondayFirst inline></Datepicker>
+    </div>
+    <div class="card-footer">
+      <span class="card-footer__icon material-icons">date_range</span>
+      <div class="card-footer__text">
+        No Events
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Datepicker from "vuejs-datepicker";
+
+export default {
+  name: 'datePicker',
+  components: { Datepicker },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
